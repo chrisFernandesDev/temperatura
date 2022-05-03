9190dbee946b5c446747cff01199f48476576322
Christina Fernandes <fernandes.christ14@gmail.com>
2022-05-03 17:02:14 -0300

    Códigos Celsius e Fahrenheit

--- app/src/main/java/com/exercicios/temperatura/MainActivity.java
+++ app/src/main/java/com/exercicios/temperatura/MainActivity.java
@@ -2,17 +2,52 @@ package com.exercicios.temperatura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnCelsius;
     private Button btnFahrenheit;
+    private EditText entradaUser;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCelsius = findViewById(R.id.btnCelsius);
+        btnFahrenheit = findViewById(R.id.btnFahrenheit);
+        entradaUser = findViewById(R.id.userDigitar);
+
+        btnCelsius.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onClick(View view) {
+                Intent intentBtnC = new Intent(getApplicationContext(), RespostaTemp.class);
+                double userEntradaUser = Double.parseDouble(entradaUser.getText().toString());
+                double resultadoCelsius = userEntradaUser * 1.8 + 32;
+                intentBtnC.putExtra("resultado", String.format("%.1f", resultadoCelsius) + " °C");
+                startActivity(intentBtnC);
+            }
+        });
+
+        btnFahrenheit.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onClick(View view) {
+                Intent intentBtnF = new Intent(getApplicationContext(), RespostaTemp.class);
+                double userEntradaUser = Double.parseDouble(entradaUser.getText().toString());
+                double resultadoFahrenheit = (userEntradaUser - 32) / 1.8;
+                intentBtnF.putExtra("resultado", String.format("%.1f", resultadoFahrenheit) + " °F");
+                startActivity(intentBtnF);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/exercicios/temperatura/RespostaTemp.java
+++ app/src/main/java/com/exercicios/temperatura/RespostaTemp.java
@@ -1,14 +1,26 @@
 package com.exercicios.temperatura;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RespostaTemp extends AppCompatActivity {
+    private TextView textResposta;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resposta_temperatura);
+
+        Intent intent = getIntent();
+        String dadosRecebidos = intent.getStringExtra("resultado");
+        Log.d("TESTE", String.valueOf(dadosRecebidos));
+
+        textResposta = findViewById(R.id.textResposta);
+        textResposta.setText(String.valueOf(dadosRecebidos));
     }
 }
