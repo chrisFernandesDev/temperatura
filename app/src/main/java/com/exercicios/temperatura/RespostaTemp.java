ebc3000c78674d19fd7ee25883f59c10d23d91c1
Christina Fernandes <fernandes.christ14@gmail.com>
2022-05-03 11:00:14 -0300

    Initial commit

---
+++ app/src/main/java/com/exercicios/temperatura/RespostaTemp.java
@@ -0,0 +1,14 @@
+package com.exercicios.temperatura;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RespostaTemp extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resposta_temperatura);
+    }
+}
