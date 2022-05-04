234b720500d66167c3d0df89590a4b71f6bc4a74
Christina Fernandes <fernandes.christ14@gmail.com>
2022-05-04 16:18:31 -0300

    implementando implements View.OnClickListener, Switch

--- app/src/main/java/com/exercicios/temperatura/MainActivity.java
+++ app/src/main/java/com/exercicios/temperatura/MainActivity.java
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnCelsius;
     private Button btnFahrenheit;
     private EditText entradaUser;
@@ -20,40 +19,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSetupView();
-        setListener();
     }
 
     private void setSetupView() {
         btnCelsius = findViewById(R.id.btnCelsius);
+        btnCelsius.setOnClickListener(this);
         btnFahrenheit = findViewById(R.id.btnFahrenheit);
+        btnFahrenheit.setOnClickListener(this);
         entradaUser = findViewById(R.id.userDigitar);
     }
 
-    private void setListener() {
-        btnCelsius.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("DefaultLocale")
-            @Override
-            public void onClick(View view) {
-                Intent intentBtnC = new Intent(getApplicationContext(), RespostaTemp.class);
-                double userEntradaUser = Double.parseDouble(entradaUser.getText().toString());
-                double resultadoCelsius = userEntradaUser * 1.8 + 32;
-                intentBtnC.putExtra("resultado", String.format("%.1f", resultadoCelsius) + " °C");
-                startActivity(intentBtnC);
-            }
-        });
+    @SuppressLint("DefaultLocale")
+    private void prepareIntentForResultC() {
+        Intent intentBtnC = new Intent(getApplicationContext(), RespostaTemp.class);
+        double userEntradaUser = Double.parseDouble(entradaUser.getText().toString());
+        double resultadoCelsius = userEntradaUser * 1.8 + 32;
+        intentBtnC.putExtra("resultado", String.format("%.1f", resultadoCelsius) + " °C");
+        startActivity(intentBtnC);
+    }
 
-        btnFahrenheit.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("DefaultLocale")
-            @Override
-            public void onClick(View view) {
-                Intent intentBtnF = new Intent(getApplicationContext(), RespostaTemp.class);
-                double userEntradaUser = Double.parseDouble(entradaUser.getText().toString());
-                double resultadoFahrenheit = (userEntradaUser - 32) / 1.8;
-                intentBtnF.putExtra("resultado", String.format("%.1f", resultadoFahrenheit) + " °F");
-                startActivity(intentBtnF);
-            }
-        });
+    @SuppressLint("DefaultLocale")
+    private void prepareIntentForResultF() {
+        Intent intentBtnF = new Intent(getApplicationContext(), RespostaTemp.class);
+        double userEntradaUser = Double.parseDouble(entradaUser.getText().toString());
+        double resultadoFahrenheit = (userEntradaUser - 32) / 1.8;
+        intentBtnF.putExtra("resultado", String.format("%.1f", resultadoFahrenheit) + " °F");
+        startActivity(intentBtnF);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnCelsius:
+                prepareIntentForResultC();
+                break;
+            case R.id.btnFahrenheit:
+                prepareIntentForResultF();
+                break;
+        }
     }
 }
-
-
